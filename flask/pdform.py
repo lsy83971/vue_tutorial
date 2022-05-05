8ad975865ac1f59b69199bd159ae312e1e88e84c
lsy <522583971@qq.com>
2022-05-05 14:13:48 +0800

    add

--- flask/pdform.py
+++ flask/pdform.py
@@ -29,3 +29,24 @@ def pd2json(tree):
 def pd2str(tree):
     return pdform(tree, f=pdformstr)
 
+def beautify(x):
+    print(type(x))
+    res = dict()
+    res["raw_data"] = pd2str(x)
+    if isinstance(x, dict):
+        res["b_data"] = f"<dict>:\n{list(x.keys())}"
+    elif isinstance(x, list):
+        res["b_data"] = f"<list>:\nlength{len(x)}"
+    elif isinstance(x, pd.DataFrame):
+        res["b_data"] = f"<pd.DataFrame>:\n{x.__repr__()}"
+    elif isinstance(x, pd.Series):
+        res["b_data"] = f"<pd.Series>:\n{x.reset_index().__repr__()}"
+    else:
+        res["b_data"] = x
+    return res
+
+def abbrStr(x):
+    if len(x) > 20:
+        return x[:17] + "..."
+    else:
+        return x
--- flask/run_server.py
+++ flask/run_server.py
@@ -3,63 +3,80 @@
 # 一切从简单 但是预留扩展空间
 import sys
 sys.path.append("/home/lsy/pyxmind")
-from flask import Flask
+from pyxmind import calcTree
+from pdform import pd2json, pd2str, beautify, abbrStr
+import pandas as pd
+import numpy as np
+import uuid
+import os
+import ast
+from flask import Flask, session
 from flask import render_template_string, render_template
 from flask import request
 import json
 app = Flask(__name__, template_folder='./',static_folder="",static_url_path="/")
+app.config['SECRET_KEY'] = os.urandom(24)
 
+class treeUserManager:
+    def __init__(self):
+        self.treeInfos = dict()
 
-from pyxmind import calcTree
-from pdform import pd2json, pd2str
-import pandas as pd
-import numpy as np
+    def create_treeInfo(self):
+        print("usename:")
+        print(session.get("username"))
+        if session.get("username", None) is None:
+            session["username"] = uuid.uuid4()
+            self.treeInfos[session["username"]] = treeInfo()
 
-def beautify(x):
-    print(type(x))
-    res = dict()
-    res["raw_data"] = pd2str(x)
-    if isinstance(x, dict):
-        res["b_data"] = f"<dict>:\n{list(x.keys())}"
-    elif isinstance(x, list):
-        res["b_data"] = f"<list>:\nlength{len(x)}"
-    elif isinstance(x, pd.DataFrame):
-        res["b_data"] = f"<pd.DataFrame>:\n{x.__repr__()}"
-    elif isinstance(x, pd.Series):
-        res["b_data"] = f"<pd.Series>:\n{x.reset_index().__repr__()}"
-    else:
-        res["b_data"] = x
-    return res
-
-def abbrStr(x):
-    if len(x) > 20:
-        return x[:17] + "..."
-    else:
-        return x
-        
+    def getTreeInfo(self):
+        self.create_treeInfo()
+        return self.treeInfos[session["username"]]
 
 class treeInfo:
     def loaddata(self, data):
         self.data = data
+
+    def loadtree(self, tree):
+        self.tree = tree
+        
+    def gettree(self):
+        return self.tree
+        
     def getdata(self):
         return self.data
-ti = treeInfo()
 
+tm = treeUserManager()
     
 def loaddata():
     data = json.loads(request.data)
+    ti = tm.getTreeInfo()
     ti.loaddata(data)
     print(data.keys())
     return "load json data success!"
 
+def code():
+    ti = tm.getTreeInfo()        
+    __code = json.loads(request.data)["code"]
+    __block = ast.parse(__code, '''tmp''', mode='exec')
+    __last = __block.body[-1]
+    __isexpr = isinstance(__last,ast.Expr)
+    _ = __block.body.pop() if __isexpr else None
+    exec(compile(__block, '''tmp''', mode='exec'))
+    output = eval(compile(ast.Expression(__last.value), '''tmp''', mode='eval')) if __isexpr else None
+    return {"output": output.__repr__()}
+    
+
 def tree():
     data = json.loads(request.data)
+    ti = tm.getTreeInfo()    
     ct = calcTree().from_treeinfo(data)
+    ti.loadtree(ct)
     res = {"err": "",
            "eroute": "",
            "route": [], 
            "name": "",
            "data": ti.getdata()}
+    
     ct.nodes["root"].receive(res)
     ##print([i for i in ct.nodes["root"]. iter()])
     res = [i for i in ct.nodes["root"]. iter()]
@@ -73,11 +90,18 @@ def tree():
             i["name_abbr"] = abbrStr(i["name"])                    
     tr = {"res": res, "node_res": node_res}
     ##tr_str = pd2str(tr)
+
+    #print("tree info:")
+    #exec("""print(ti.tree.nodes)""")
+    #print(ti.tree.nodes['root']. stack)
+    
     return json.dumps(tr)
 
 ## app.add_url_rule("/", "init", init, methods=["GET", "POST"])
 app.add_url_rule("/flask/loaddata", "loaddata", loaddata, methods=["GET", "POST"])
 app.add_url_rule("/flask/tree", "tree", tree, methods=["GET", "POST"])
+app.add_url_rule("/flask/code", "code", code, methods=["GET", "POST"])
+
 
 
 if __name__ == "__main__":
