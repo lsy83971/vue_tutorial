afb3ddcec54e276d409f93cb63bf115c852e163c
lsy <522583971@qq.com>
2022-05-06 17:52:16 +0800

    yes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="node_syntax">
+    <h1>This is an about framework</h1>
+  </div>
+  
+  <div class="node_syntax">
+    <h1>This is an about node syntax</h1>
+  </div>
+
+  
+  <div class="node_syntax">
+    <h1>This is an about canvas</h1>
+  </div>
+
+  <div class="node_syntax">
+    <h1>This is an about code editor</h1>
+  </div>
+  
+  <div class="hotkey">
+    <h1>This is an about hotkey</h1>
+    <div>addNode:a</div>
+    <div>
+      addSurf:s
+      deleteNode:d | Delete
+    </div>
   </div>
 </template>
